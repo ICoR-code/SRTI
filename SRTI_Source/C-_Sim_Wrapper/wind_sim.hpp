5d868a971448a7a1d2fa25c33b5e84c48ac6acea
Hao Lu <harveylu@umich.edu>
2018-07-02 15:49:59 -0400

    Oops. Forgot to add unstaged files.

---
+++ SRTI_Source/C-_Sim_Wrapper/wind_sim.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+#include "rapidjson/document.h"
+
+using namespace std;
+
+class WindSim {
+public:
+    WindSim();
+    ~WindSim();
+
+    void simulate();
+
+    rapidjson::Value & getMessage(string);
+    void setMessage(string, rapidjson::Value &);
+
+    void generateInitialMessage();
+
+private:
+    map<string, rapidjson::Value &> input;
+    map<string, rapidjson::Value &> output;
+
+    map<string, int> history_size;
+
+    map<string, vector<rapidjson::Value> > history;
+
+    void updateHistory();
+
+};
